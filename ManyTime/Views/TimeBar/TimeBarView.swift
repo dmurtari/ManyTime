36c9372f723fea33268bb3ac3d325d3b133ff304
Domenic Murtari <domenic@kazusan.me>
2026-04-21 20:17:23 +0900

    Fix sizing

--- ManyTime/Views/TimeBar/TimeBarView.swift
+++ ManyTime/Views/TimeBar/TimeBarView.swift
@@ -20,121 +20,119 @@ struct TimeBarView: View {
     @State private var isInternalScroll = false
 
     var body: some View {
-        ScrollViewReader { proxy in
-            ScrollView([.horizontal]) {
-                LazyHStack(spacing: 0) {
-                    ForEach(dateArray, id: \.timeIntervalSince1970) { date in
-                        TimeBarTimeView(
-                            date: .constant(date),
-                            dimension: Int(Constants.TimeBarConstants.timeViewSide),
-                            timeZone: timeZone,
-                            showDate: getHour(from: date) == 0
-                        )
-                        .id(date)
-                        .clipShape(
-                            getHour(from: date) == 23
-                                ? AnyShape(
-                                    UnevenRoundedRectangle(cornerRadii: .init(bottomTrailing: 6, topTrailing: 6))
-                                )
-                                : AnyShape(
-                                    Rectangle()
-                                )
-                        )
-                        .clipShape(
-                            getHour(from: date) == 0
-                                ? AnyShape(
-                                    UnevenRoundedRectangle(cornerRadii: .init(topLeading: 6, bottomLeading: 6))
-                                )
-                                : AnyShape(
-                                    Rectangle()
-                                )
-                        )
-                        .onAppear {
-                            let thresholdIndex = dateArray.index(dateArray.endIndex, offsetBy: -5)
+        ScrollView([.horizontal]) {
+            LazyHStack(spacing: 0) {
+                ForEach(dateArray, id: \.timeIntervalSince1970) { date in
+                    TimeBarTimeView(
+                        date: .constant(date),
+                        dimension: Int(Constants.TimeBarConstants.timeViewSide),
+                        timeZone: timeZone,
+                        showDate: getHour(from: date) == 0
+                    )
+                    .id(date)
+                    .clipShape(
+                        getHour(from: date) == 23
+                            ? AnyShape(
+                                UnevenRoundedRectangle(cornerRadii: .init(bottomTrailing: 6, topTrailing: 6))
+                            )
+                            : AnyShape(
+                                Rectangle()
+                            )
+                    )
+                    .clipShape(
+                        getHour(from: date) == 0
+                            ? AnyShape(
+                                UnevenRoundedRectangle(cornerRadii: .init(topLeading: 6, bottomLeading: 6))
+                            )
+                            : AnyShape(
+                                Rectangle()
+                            )
+                    )
+                    .onAppear {
+                        let thresholdIndex = dateArray.index(dateArray.endIndex, offsetBy: -5)
 
-                            if dateArray.firstIndex(of: date) == thresholdIndex {
-                                appendDates()
-                            }
+                        if dateArray.firstIndex(of: date) == thresholdIndex {
+                            appendDates()
                         }
-                        .onAppear {
-                            let thresholdIndex = dateArray.index(dateArray.startIndex, offsetBy: 5)
+                    }
+                    .onAppear {
+                        let thresholdIndex = dateArray.index(dateArray.startIndex, offsetBy: 5)
 
-                            if dateArray.firstIndex(of: date) == thresholdIndex {
-                                prependDates()
-                            }
+                        if dateArray.firstIndex(of: date) == thresholdIndex {
+                            prependDates()
                         }
                     }
                 }
             }
-            .onScrollGeometryChange(for: CGFloat.self) { geo in
-                geo.contentOffset.x
-            } action: { _, newX in
-                currentScrollX = newX
+        }
+        .onScrollGeometryChange(for: CGFloat.self) { geo in
+            geo.contentOffset.x
+        } action: { _, newX in
+            currentScrollX = newX
 
-                guard !isProgrammaticScroll else {
-                    return
-                }
-
-                guard let startTime = dateArray.first else {
-                    return
-                }
-
-                let pixelsPerHour = Constants.TimeBarConstants.timeViewSide
-                let viewWidth = Constants.AppViewConstants.timeMenuWidth
-                let contentOffsetAtCenter = currentScrollX + viewWidth / 2
-                let secondsFromStart = (contentOffsetAtCenter / pixelsPerHour) * 3600
-                let scrollTime = startTime.addingTimeInterval(TimeInterval(secondsFromStart))
-
-                isInternalScroll = true
-                currentTime = scrollTime
-                isInternalScroll = false
+            guard !isProgrammaticScroll else {
+                return
             }
-            .scrollIndicators(.hidden)
-            .frame(
-                width: Constants.AppViewConstants.timeMenuWidth,
-                height: Constants.TimeBarConstants.timeViewSide
-            )
-            .overlay {
-                Path { path in
-                    let x = Constants.AppViewConstants.timeMenuWidth / 2 + Constants.TimeBarConstants.timeViewSide / 2
 
-                    path.move(to: CGPoint(x: x, y: -1))
-                    path.addLine(to: CGPoint(x: x, y: Constants.TimeBarConstants.timeViewSide + 1))
-                }
-                .stroke(.black, lineWidth: 2)
+            guard let startTime = dateArray.first else {
+                return
             }
-            .onAppear {
-                dateArray.append(currentTime)
-                appendDates()
-                prependDates()
 
-                let initialScrollX =
-                    CGFloat(Constants.TimeBarConstants.timeViewInitialCount) * Constants.TimeBarConstants.timeViewSide
-                    - Constants.AppViewConstants.timeMenuWidth / 2
+            let pixelsPerHour = Constants.TimeBarConstants.timeViewSide
+            let viewWidth = Constants.AppViewConstants.timeMenuWidth
+            let contentOffsetAtCenter = currentScrollX + viewWidth / 2
+            let secondsFromStart = (contentOffsetAtCenter / pixelsPerHour) * 3600
+            let scrollTime = startTime.addingTimeInterval(TimeInterval(secondsFromStart))
 
-                position = ScrollPosition(x: initialScrollX)
+            isInternalScroll = true
+            currentTime = scrollTime
+            isInternalScroll = false
+        }
+        .scrollIndicators(.hidden)
+        .frame(
+            width: Constants.AppViewConstants.timeMenuWidth,
+            height: Constants.TimeBarConstants.timeViewSide
+        )
+        .overlay {
+            Path { path in
+                let x = Constants.AppViewConstants.timeMenuWidth / 2 + Constants.TimeBarConstants.timeViewSide / 2
+
+                path.move(to: CGPoint(x: x, y: -1))
+                path.addLine(to: CGPoint(x: x, y: Constants.TimeBarConstants.timeViewSide + 1))
             }
-            .defaultScrollAnchor(.bottomLeading)
-            .scrollPosition($position)
-            .onChange(of: currentTime) { _, newValue in
-                guard !isInternalScroll else {
-                    return
-                }
+            .stroke(.black, lineWidth: 2)
+        }
+        .onAppear {
+            dateArray.append(currentTime)
+            appendDates()
+            prependDates()
 
-                guard let startTime = dateArray.first else {
-                    return
-                }
+            let initialScrollX =
+                CGFloat(Constants.TimeBarConstants.timeViewInitialCount) * Constants.TimeBarConstants.timeViewSide
+                - Constants.AppViewConstants.timeMenuWidth / 2
 
-                let pixelsPerHour = Constants.TimeBarConstants.timeViewSide
-                let viewWidth = Constants.AppViewConstants.timeMenuWidth
-                let secondsFromStart = newValue.timeIntervalSince(startTime)
-                let targetScrollX = CGFloat(secondsFromStart / 3600) * pixelsPerHour - viewWidth / 2
+            position = ScrollPosition(x: initialScrollX)
+        }
+        .defaultScrollAnchor(.bottomLeading)
+        .scrollPosition($position)
+        .onChange(of: currentTime) { _, newValue in
+            guard !isInternalScroll else {
+                return
+            }
 
-                isProgrammaticScroll = true
-                position = ScrollPosition(x: targetScrollX)
-                DispatchQueue.main.async {
-                    self.isProgrammaticScroll = false
-                }
+            guard let startTime = dateArray.first else {
+                return
+            }
+
+            let pixelsPerHour = Constants.TimeBarConstants.timeViewSide
+            let viewWidth = Constants.AppViewConstants.timeMenuWidth
+            let secondsFromStart = newValue.timeIntervalSince(startTime)
+            let targetScrollX = CGFloat(secondsFromStart / 3600) * pixelsPerHour - viewWidth / 2
+
+            isProgrammaticScroll = true
+            position = ScrollPosition(x: targetScrollX)
+            DispatchQueue.main.async {
+                self.isProgrammaticScroll = false
             }
         }
     }
